0dce204444bbbeda9b5d5b4646aa52540a33accd
Thudilie <thuyanh.nguyen.de@gmail.com>
2020-04-10 23:57:16 +0200

    Create react app

---
+++ app/src/pages/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Home from './Home';
+
+const App = () => (
+  <>
+    <header>
+    </header>
+    <main>
+      <Home/>
+    </main>
+    <footer>
+    </footer>
+  </>
+)
+
+export default App;
---
+++ app/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => (
+  <>
+    <h1>Home</h1>
+    <div>
+      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+      eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+    </div>
+  </>
+)
+
+export default Home;
