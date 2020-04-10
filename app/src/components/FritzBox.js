24e313e1d43ab3af0d427b62fb3fcc31c0b4dbf6
Thudilie <thuyanh.nguyen.de@gmail.com>
2020-04-11 00:06:30 +0200

    Add button for state change

---
+++ app/src/components/FritzBox.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+
+const FritzBox = ({articleName, setArticleInfo}) => {
+  const[load,setloading] = useState('start loading');
+  const extractfritzbox = async() =>{
+    setloading('finish loading')
+  }
+  return (
+    <>
+      <h2>start extracting fritz box</h2>
+        <p>{load}</p>
+        <button onClick={()=>extractfritzbox()}>start</button>
+    </>
+   );
+}
+
+export default FritzBox;
