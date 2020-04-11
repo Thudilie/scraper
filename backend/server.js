5eca905c6435aae629862c05ec12c76735b62c60
Thudilie <thuyanh.nguyen.de@gmail.com>
2020-04-11 02:21:20 +0200

    Add download script

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,25 @@
 import express from 'express'
+import cmd from 'node-cmd'
 
 const app = express();
 
-app.get('/helloworld', (req, res) => res.send('HelloWorld'))
+app.get('/helloworld', (req, res) => {
+  cmd.get(
+    `
+        cd ..
+        npm start
+        ls
+    `,
+      function(err, data, stderr){
+          if (!err) {
+            console.log('the node-cmd cloned dir contains these files :\n\n',data)
+          } else {
+            console.log('error', err)
+          }
+
+      }
+  );
+  res.send('HelloWorld')
+})
 
 app.listen(8000, () => { console.log('listening on port 8000')})
