605ddd32d256acc89b82e76de17854ffa9702fce
Thudilie <thuyanh.nguyen.de@gmail.com>
2020-04-12 00:12:18 +0200

    Add loading gif timer, disable button on loading

--- app/src/components/FritzBox.js
+++ app/src/components/FritzBox.js
@@ -1,22 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
+import './../css/FritzBox.css'
 
 const FritzBox = () => {
   const[load,setloading] = useState('start loading');
-  const[loadingGif,setloadingGif] = useState('');
+  const[loadingGif, setloadingGif] = useState('finish');
+  const[time, setTime] = useState(0);
+  const[starttimer, setStarttimer] = useState(false);
 
   const extractfritzbox = async () => {
+    setTime(0);
     setloading('download started');
     setloadingGif('load')
+    setStarttimer(true)
     const result = await fetch(`/fritzbox`)
     const body = await result.json();
     setloadingGif('finish')
     setloading(body.status);
+    setStarttimer(false)
   }
+
+  useEffect(() => {
+    if(starttimer){
+      setTimeout(() => {
+        setTime(time+1);
+      }, 1000);
+    }
+  },[starttimer,time]);
+
   return (
     <>
       <h2>start extracting fritz box</h2>
-        <p className={loadingGif}>{load}</p>
-        <button onClick={()=>extractfritzbox()}>start</button>
+        <div>{time}</div>
+        <div className={loadingGif}></div>
+        <p>{load}</p>
+        <button disabled={starttimer} onClick={()=>extractfritzbox()}>start</button>
     </>
    );
 }
