177480ecb785c6780ce8d16ffffaefc2f76f55b2
Thudilie <thuyanh.nguyen.de@gmail.com>
2020-04-12 02:01:55 +0200

    Shorten fritzbox code

--- app/src/components/FritzBox.js
+++ app/src/components/FritzBox.js
@@ -2,38 +2,32 @@ import React, { useState, useEffect} from 'react';
 import './../css/FritzBox.css'
 
 const FritzBox = () => {
-  const[load,setloading] = useState('start loading');
-  const[loadingGif, setloadingGif] = useState('finish');
+  const[load,setloading] = useState({'message':'start loading','gif':'finish', 'timer': false});
   const[time, setTime] = useState(0);
-  const[starttimer, setStarttimer] = useState(false);
 
   const extractfritzbox = async () => {
     setTime(0);
-    setloading('download started');
-    setloadingGif('load')
-    setStarttimer(true)
+    setloading({'message':'download started','gif':'load', 'timer': true});
     const result = await fetch(`/fritzbox`)
     const body = await result.json();
-    setloadingGif('finish')
-    setloading(body.status);
-    setStarttimer(false)
+    setloading({'message': body.status,'gif':'finish','timer':false});
   }
 
   useEffect(() => {
-    if(starttimer){
+    if(load.timer){
       setTimeout(() => {
         setTime(time+1);
       }, 1000);
     }
-  },[starttimer,time]);
+  },[load.timer,time]);
 
   return (
     <>
       <h2>start extracting fritz box</h2>
         <div>{time}</div>
-        <div className={loadingGif}></div>
-        <p>{load}</p>
-        <button disabled={starttimer} onClick={()=>extractfritzbox()}>start</button>
+        <div className={load.gif}></div>
+        <p>{load.message}</p>
+        <button disabled={load.timer} onClick={()=>extractfritzbox()}>start</button>
     </>
    );
 }
